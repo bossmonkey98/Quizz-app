abb5746406a894e67914ccfcc25c708d16817916
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-05-19 18:16:32 +0530

    minor changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import {useAuth} from '../../context/AuthContext'
+import {LogoutHandler} from "../../services/authandlers"
 
 const Navbar = () => {
+  const {user ,setUser} = useAuth()
   return (
       <nav className="navbar">
         <NavLink to="/" className="app-logo">
           <h1 className="logo-txt">Quizzy</h1>
         </NavLink>
+        {!user.isUserLoggedIn?
         <NavLink to="/login" className="auth">
           <button className="btn">Login</button>
-        </NavLink>
+        </NavLink>:
+        <div className="auth">
+        <button className="btn" onClick={()=>LogoutHandler(setUser)}>Logout</button>
+        </div>
+        }
       </nav>
   );
 };
